5c792089ace9a93617d8ea20939afbb4a8a6678b
Davi Araujo <davi.amaral.araujo@gmail.com>
2021-03-19 09:44:41 -0300

    done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const { v4: uuidv4, validate } = require('uuid');
+const { use } = require('../../ignite-desafio01/src');
 
 const app = express();
 app.use(express.json());
@@ -10,20 +11,74 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
-}
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user){
+    return response.status(404).json({error:"User not found"});
+  }
+
+  request.user = user;
+  return next();
+};
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+
+  const todoAmount = user.todos.length
+
+  if (user.pro == false){
+    if(todoAmount == 10){
+      return response.status(403).json({ error:"Not available on free plan"});
+    }
+    else{
+      return next();
+    }
+  }
+  else{
+    return next();
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+
+  const uuidValidator = validate(id);
+  if(!uuidValidator){
+    return response.status(400).json({ error: "Not a valid id"});
+  };
+
+  const user = users.find(user => user.username === username);
+
+  if (!user){
+    return response.status(404).json({error:"User not found"});
+  };
+
+  const todo = user.todos.find((todo) =>todo.id ===id);
+  if(!todo){
+    return response.status(404).json({error:"Todo not exists"});
+  }
+
+  request.user = user;
+  request.todo = todo;
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
-}
+  const {id} = request.params;
+
+  const user = users.find((user) => user.id === id);
+  if (!user) {
+    return response.status(404).json({error:"User not found"});
+  };
+
+  request.user = user;
+  return next();
+};
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
